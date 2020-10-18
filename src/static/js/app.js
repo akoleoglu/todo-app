0309cca5631ae4558594f67ceaf0c40ddeb971cd
root <root@ip-172-31-51-81.ec2.internal>
2020-10-18 00:19:58 +0000

    button changed

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Click here to add new task'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
